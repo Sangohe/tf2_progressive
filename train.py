6b0a3ee2b74431f3f5ddf8d652dbe3ea3ce850ac
Sangohe <santiagogomez.h@outlook.com>
2020-03-26 16:05:23 -0500

    modify cuda_visible_devices for tests in colab

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
  
 # The GPU id to use, usually either "0" or "1";
-os.environ["CUDA_VISIBLE_DEVICES"] = "";  
+os.environ["CUDA_VISIBLE_DEVICES"] = "0";  
 
 # Libraries
 import time
